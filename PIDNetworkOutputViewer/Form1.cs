ce639f8bdc77b3474e3e0b9e2ff9179f8addb853
zhu-ty <zhu-ty@outlook.com>
2018-08-10 14:29:14 +0800

    first commit

---
+++ PIDNetworkOutputViewer/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIDNetworkOutputViewer
+{
+    public partial class Form1 : Form
+    {
+        //learning_rate P I D train_lossf val_lossf train_accf val_accf
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
